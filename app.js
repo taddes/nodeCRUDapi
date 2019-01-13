d0c4a100d77f103fd6aee45ddcff8ca79efe977c
Taddes Korris <taddes.korris@gmail.com>
2019-01-13 17:12:05 -0500

    cors mitigation, full paths for products and orders

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.use((res, req, next) => {
+  res.header('Access=Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    // Browser always sends an options request first when sending a POST or PUT REQ
+    // Unavoidable. Browser checks if you can 
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({})
+    }
+});
+
 // sets up middleware where all requests filtered thru
 app.use('/products', productRoutes);
-
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
