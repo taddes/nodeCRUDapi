c05f6a73a6b720786d3e2e930edb9337af3da165
Taddes Korris <taddes.korris@gmail.com>
2019-02-02 16:58:26 -0500

    auth login with email

--- api/routes/user.js
+++ api/routes/user.js
@@ -45,6 +45,39 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
+router.post('/login', (req, res, next) => {
+  User.find({
+    email: req.body.email
+  })
+  .exec()
+  .then(user => {
+    if (user.length < 1) {
+      return res.status(401).json({
+        message: 'Auth Failed'
+      });
+    } 
+    bcrypt.compate(req.body.password, user[0].password, (err, result) => {
+      if (err) {
+        return res.status(401).json({
+          message: 'Auth Failed'
+        });
+      }
+      if (result) {
+        return res.status(200).json({
+          message: 'Auth successful'
+        })
+      }
+    });
+
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error: err
+    })
+  })
+})
+
 router.delete('/:userId', (req, res, next) => {
   User.remove({ _id: req.params.userId})
   .exec()
